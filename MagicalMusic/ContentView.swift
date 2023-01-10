6bda0dd6f598134792b6ebcd6be5206ce63a681c
likai <1195419506@qq.com>
2023-01-11 07:22:44 +0800

    Initial Commit

---
+++ MagicalMusic/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MagicalMusic
+//
+//  Created by 李凯 on 2023/1/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MagicalMusic/MagicalMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicalMusicApp.swift
+//  MagicalMusic
+//
+//  Created by 李凯 on 2023/1/11.
+//
+
+import SwiftUI
+
+@main
+struct MagicalMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
